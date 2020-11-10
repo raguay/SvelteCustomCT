c62bc821196c11a12876d8f1c6602f8d10d412c3
raguay <raguay@customct.com>
2020-11-10 20:57:50 +0700

    updating dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
+import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import { mdsvex } from 'mdsvex';
 
@@ -28,7 +27,7 @@ export default {
 			// we'll extract any component CSS out into
 			// a separate file — better for performance
 			css: css => {
-				css.write('public/bundle.css');
+				css.write('bundle.css');
 			}
 		}),
 
@@ -37,10 +36,6 @@ export default {
 		// some cases you'll need additional configuration —
 		// consult the documentation for details:
 		// https://github.com/rollup/rollup-plugin-commonjs
-		resolve({
-			browser: true,
-			dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
-		}),
 		commonjs(),
 
 		// Watch the `public` directory and refresh the
